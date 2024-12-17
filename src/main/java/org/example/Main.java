c9fae111ebcc63cacf97fb90ea24a9da65aaee7f
shimaIX <syefye1316@gmail.com>
2024-12-17 11:15:00 +0800

    Created a main menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Hospital Billing System");
+        User_Interface.Main_Menu();
     }
 }
