ff6b2ec8de470da154c904ee51733c3eb98d8630
recca383 <sirpatrick121402@gmail.com>
2024-12-21 13:40:33 +0800

    Added Interfaces for better segragation

--- src/main/java/org/example/Interfaces/IDatabaseServices.java
+++ src/main/java/org/example/Interfaces/IDatabaseServices.java
@@ -1,4 +1,8 @@
 package org.example.Interfaces;
 
 public interface IDatabaseServices {
+    public boolean InsertToDatabase(String Name, int Age, int Gender, String ContactInformation);
+    public boolean UpdateDatabase(String FirstName, String LastName, String Specialty);
+    public boolean DeleteDatabase(String ID);
+
 }
